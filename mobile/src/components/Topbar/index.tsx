83760035a63fa0d13b895a9fc36cd78edc09ef04
jbdeb <jean-baptiste.debize@epitech.eu>
2024-04-20 16:38:09 -0700

    fix top bar

--- mobile/src/components/Topbar/index.tsx
+++ mobile/src/components/Topbar/index.tsx
@@ -3,8 +3,10 @@ import EARTH from "../../../assets/globe.png";
 // @ts-ignore
 import PROFILE from "../../../assets/icons/profile.png";
 // @ts-ignore
+
 import NAVBAR from "../../../assets/nav.png";
 // @ts-ignore
+
 import CAMERA from "../../../assets/camera.png";
 // @ts-ignore
 import GARDEN from "../../../assets/garden.png";
@@ -23,103 +25,41 @@ function TopBar() {
   const navigateToPath = (path: string) => () => navigate(path);
 
   return (
-    <View
-      style={{
-        display: "flex",
-        width: "100%",
-        height: 85,
-        position: "absolute",
-        bottom: 50,
-      }}
-    >
-      <ImageBackground
-        style={{
-          marginLeft: 15,
-          marginTop: -5,
-          opacity: 0.4,
-          paddingLeft: 15,
-          paddingRight: 15,
-          position: "absolute",
-          width: "100%",
-          height: "110%",
-        }}
-        source={NAVBAR}
-      />
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          paddingTop: 17,
-          paddingLeft: 5,
-          paddingRight: 25,
-        }}
-      >
+    <View style={{
+      display: 'flex',
+      width: '100%',
+      height: 85,
+      position: 'absolute',
+      bottom: 50
+    }}>
+      <ImageBackground style={{marginLeft: 15, marginTop: -5, opacity: 0.4, paddingLeft: 15, paddingRight: 15, position: 'absolute', width: '100%', height: '110%' }} resizeMode="fit" source={NAVBAR}/>    
+      <View style={{
+        display: 'flex',
+        flexDirection: 'row',
+        paddingTop: 17,
+        paddingLeft: 5,
+        paddingRight: 25
+      }}>
         <CustomIconButton onPress={navigateToPath("/")}>
           <Box marginLeft="10px" height="40px" width="40px">
-            <Image
-              style={{ position: "absolute", height: "100%", width: "100%" }}
-              source={EARTH}
-            ></Image>
-            <Text
-              style={{
-                height: "100%",
-                transform: "translateY(40px)",
-                fontWeight: "bold",
-                color: "white",
-                textAlign: "center",
-              }}
-            >
+            <Image style={{ position: 'absolute', height: "100%", width: "100%" }} source={EARTH}></Image>
+            <Text style={{height: '100%', transform: 'translateY(40px)', fontWeight: 'bold', color: 'white', textAlign: 'center'}}>
               Map
             </Text>
           </Box>
         </CustomIconButton>
         <CustomIconButton onPress={navigateToPath("/camera")}>
-          <Box
-            backgroundColor="#8DDC8B"
-            borderWidth="1px"
-            borderColor="black"
-            borderRadius="100"
-            marginTop="-25px"
-            marginLeft="51px"
-            height="60px"
-            width="60px"
-          >
-            <ImageBackground
-              resizeMode="contain"
-              style={{ position: "absolute", height: "100%", width: "100%" }}
-              source={CAMERA}
-            ></ImageBackground>
-            <Text
-              style={{
-                height: "100%",
-                transform: "translateY(65px)",
-                fontWeight: "bold",
-                color: "white",
-                textAlign: "center",
-              }}
-            >
+          <Box backgroundColor="#8DDC8B" borderWidth='1px' borderColor="black" borderRadius="100" marginTop="-25px" marginLeft="51px" height="60px" width="60px">
+            <ImageBackground resizeMode="contain" style={{ position: 'absolute', height: "100%", width: "100%" }} source={CAMERA}></ImageBackground>
+            <Text style={{height: '100%', transform: 'translateY(65px)', fontWeight: 'bold', color: 'white', textAlign: 'center'}}>
               Scan
             </Text>
           </Box>
         </CustomIconButton>
         <CustomIconButton onPress={navigateToPath("/collection")}>
           <Box borderRadius="100" marginLeft="30px" height="40px" width="40px">
-            <ImageBackground
-              resizeMode="contain"
-              style={{ position: "absolute", height: "100%", width: "100%" }}
-              source={GARDEN}
-            ></ImageBackground>
-            <Text
-              style={{
-                height: "100%",
-                marginLeft: -6,
-                width: 51,
-                transform: "translateY(40px)",
-                color: "white",
-                fontWeight: "bold",
-                textAlign: "center",
-              }}
-            >
+            <ImageBackground resizeMode="contain" style={{ position: 'absolute', height: "100%", width: "100%" }} source={GARDEN}></ImageBackground>
+            <Text style={{height: '100%', marginLeft: -6, width: 51, transform: 'translateY(40px)', color: 'white', fontWeight: 'bold', textAlign: 'center'}}>
               Garden
             </Text>
           </Box>
