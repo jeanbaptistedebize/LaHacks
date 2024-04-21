888cbea22747d267adc556a05dbb190ebbf03b42
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2024-04-20 23:37:17 -0700

    backend fully implemented in node. Flask app to run leaf classify and disease classify.

--- nodetest/backend.js
+++ nodetest/backend.js
@@ -10,7 +10,7 @@ function encodeImageToBase64(filePath) {
 }
 
 
-async function generateImage() {
+async function geminiClassify() {
     // get API_KEY from environment
     const API_KEY = process.env.API_KEY;
     const genAI = new GoogleGenerativeAI(API_KEY);
@@ -22,11 +22,11 @@ async function generateImage() {
     //const prompt = "You will return a json response of the common name and scientific name of the flower in the image.";
 
     const prompt = `
-    You are given an image of a plant. Your response must be a JSON object containing 13 fields that are all strings. Here is what you need to return: 
+    You are given an image of a plant. Your response must be a JSON object containing 8 fields that are all strings. One of the important fields is isleaf. If the picture is a closeup of a leaf, this field should be true. Otherwise it should be false. In addition, disease should always be "healthy". Here is what you need to return: 
 
-    commonname, scientificname, kingdom, subkingdom, superdivision, division, class, sublcass, order, family, genus, species, description. 
+    commonname, scientificname, isleaf, family, genus, species, description, disease. 
 
-    The description should be three sentences. Do not return null for any fields. If you are not sure, make a good guess. 
+    The description should be three sentences. Do not return null for any fields. If you are not sure, make a good guess. Do not include backticks or the word json. The response should begin with { and end with }.
 
     `
 
@@ -38,14 +38,50 @@ async function generateImage() {
     };
 
 
-    const result = await model.generateContent([prompt, image]);
-    console.log(result.response.text());
+    let result = await model.generateContent([prompt, image]);
+    // change result from text to json
+    result = JSON.parse(result.response.text());
+
+    console.log("is a leaf?: ", result['isleaf']);
+    if (result['isleaf'] == true) {
+        console.log("the image is a leaf, querying additional information...");
+        const species = await predictLeafSpecies(base64image);
+        const disease = await isDisease(base64image);
+        const supplement = await geminiSupplement(species, disease);
+        return supplement;
+    } else { 
+        console.log("the image is not a leaf, returning...\n");
+        return result;
+    }
 }
-//generateImage();
-//o
+
+async function geminiSupplement(species, disease) {
+    // get API_KEY from environment
+    const API_KEY = process.env.API_KEY;
+    const genAI = new GoogleGenerativeAI(API_KEY);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    //const prompt = "You will return a json response of the common name and scientific name of the flower in the image.";
+
+    const prompt = `
+    You are given a species: ${species}, and a disease: ${disease}. Your response must be a JSON object containing 8 fields that are all strings. isleaf should be set to true or false. Here is what you need to return:
+
+    commonname, scientificname, isleaf, family, genus, species, description, disease. 
+
+    The description should be three sentences. Do not return null for any fields. If you are not sure, make a good guess. Do not include backticks or the word json. The response should begin with { and end with }.
+
+    `
+
+    let result = await model.generateContent(prompt);
+    result = JSON.parse(result.response.text());
+    // const response = await result.response.text();
+
+    return result;
+}
 
 async function predictLeafSpecies(imageBase64) {
-    const url = ' http://127.0.0.1:5000';  // URL of the Flask server
+    const url = 'http://127.0.0.1:5000/predict';  // URL of the Flask server
     const data = {
         image: imageBase64
     };
@@ -64,13 +100,66 @@ async function predictLeafSpecies(imageBase64) {
         }
 
         const result = await response.json();
-        console.log('Predicted species:', result.species);
         return result.species;
     } catch (error) {
         console.error('Error predicting species:', error);
     }
 }
 
-const imagePath = 'sunflower.jpg';
-const base64image = encodeImageToBase64(imagePath);
-console.log(await predictLeafSpecies(base64image));
+async function isDisease(imageBase64) {
+    const url = 'http://127.0.0.1:5000/disease';  // URL of the Flask server
+    const data = {
+        image: imageBase64
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        // go through the dictionary, find the key with the highest value
+        // if it is greater than .5, return the key, else return 'healthy'
+
+        let max = 0;
+        let maxKey = '';
+        for (const key in result) {
+            if (result[key] > max) {
+                max = result[key];
+                maxKey = key;
+            }
+        }
+
+        if (max > 0.5) {
+            result = maxKey;
+            return maxKey;
+        } else { 
+            return 'healthy';
+        }
+
+    } catch (error) {
+        console.error('Error predicting species:', error);
+    }
+}
+
+console.log("Beginning Gemini Classify");
+console.log(await geminiClassify(), "\n");
+
+//const imagePath = 'sunflower.jpg';
+//const base64image = encodeImageToBase64(imagePath);
+//console.log("leaf classify: ", await predictLeafSpecies(base64image));
+//console.log("disease classify: ", await isDisease(base64image));
+
+// How does this work? 
+    // Gemini classify is called. If the image is a leaf, it will call predictLeafSpecies and isDisease to get the species and disease of the leaf.
+    // predictleafspecies holds a much stronger model than the gemini model, so it is used to predict the species of the leaf.
+    // isDisease uses a model to predict the disease of the leaf. It returns the disease if the confidence is greater than 0.5, otherwise it returns 'healthy'.
+    // json is returned with the species and disease of the leaf.
---
+++ plantmodelstest/flaskapp/app.py
@@ -0,0 +1,91 @@
+import io
+import base64
+from flask import Flask, request, jsonify
+import torch
+from torchvision import models, transforms, datasets
+from torch import nn
+from PIL import Image
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "*"}})  # Explicitly allowing all origins for the /predict route
+
+# Load the model
+model_path = '/Users/evancoons/Downloads/model2.pth'  # Specify the path to your saved model weights
+model = models.resnet50()  # Initialize the ResNet50 model
+num_classes = 184  # Set the number of your output classes
+model.fc = nn.Linear(model.fc.in_features, num_classes)  # Adjust the fully connected layer
+model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+model.eval()
+
+# Define the image transformations
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# dataset_path = '/Users/evancoons/Downloads/leafsnap-dataset/dataset/images/field'
+# dataset = datasets.ImageFolder(dataset_path, transform=transform)
+# class_labels = dataset.classes
+class_labels = ['abies concolor', 'abies nordmanniana', 'acer campestre', 'acer ginnala', 'acer griseum', 'acer negundo', 'acer palmatum', 'acer pensylvanicum', 'acer platanoides', 'acer pseudoplatanus', 'acer rubrum', 'acer saccharinum', 'acer saccharum', 'aesculus flava', 'aesculus glabra', 'aesculus hippocastamon', 'aesculus pavi', 'ailanthus altissima', 'albizia julibrissin', 'amelanchier arborea', 'amelanchier canadensis', 'amelanchier laevis', 'asimina triloba', 'betula alleghaniensis', 'betula jacqemontii', 'betula lenta', 'betula nigra', 'betula populifolia', 'broussonettia papyrifera', 'carpinus betulus', 'carpinus caroliniana', 'carya cordiformis', 'carya glabra', 'carya ovata', 'carya tomentosa', 'castanea dentata', 'catalpa bignonioides', 'catalpa speciosa', 'cedrus atlantica', 'cedrus deodara', 'cedrus libani', 'celtis occidentalis', 'celtis tenuifolia', 'cercidiphyllum japonicum', 'cercis canadensis', 'chamaecyparis pisifera', 'chamaecyparis thyoides', 'chionanthus retusus', 'chionanthus virginicus', 'cladrastis lutea', 'cornus florida', 'cornus kousa', 'cornus mas', 'corylus colurna', 'crataegus crus-galli', 'crataegus laevigata', 'crataegus phaenopyrum', 'crataegus pruinosa', 'crataegus viridis', 'cryptomeria japonica', 'diospyros virginiana', 'eucommia ulmoides', 'evodia daniellii', 'fagus grandifolia', 'ficus carica', 'fraxinus americana', 'fraxinus nigra', 'fraxinus pennsylvanica', 'ginkgo biloba', 'gleditsia triacanthos', 'gymnocladus dioicus', 'halesia tetraptera', 'ilex opaca', 'juglans cinerea', 'juglans nigra', 'juniperus virginiana', 'koelreuteria paniculata', 'larix decidua', 'liquidambar styraciflua', 'liriodendron tulipifera', 'maclura pomifera', 'magnolia acuminata', 'magnolia denudata', 'magnolia grandiflora', 'magnolia macrophylla', 'magnolia soulangiana', 'magnolia stellata', 'magnolia tripetala', 'magnolia virginiana', 'malus angustifolia', 'malus baccata', 'malus coronaria', 'malus floribunda', 'malus hupehensis', 'malus pumila', 'metasequoia glyptostroboides', 'morus alba', 'morus rubra', 'nyssa sylvatica', 'ostrya virginiana', 'oxydendrum arboreum', 'paulownia tomentosa', 'phellodendron amurense', 'picea abies', 'picea orientalis', 'picea pungens', 'pinus bungeana', 'pinus cembra', 'pinus densiflora', 'pinus echinata', 'pinus flexilis', 'pinus koraiensis', 'pinus nigra', 'pinus parviflora', 'pinus peucea', 'pinus pungens', 'pinus resinosa', 'pinus rigida', 'pinus strobus', 'pinus sylvestris', 'pinus taeda', 'pinus thunbergii', 'pinus virginiana', 'pinus wallichiana', 'platanus acerifolia', 'platanus occidentalis', 'populus deltoides', 'populus grandidentata', 'populus tremuloides', 'prunus pensylvanica', 'prunus sargentii', 'prunus serotina', 'prunus serrulata', 'prunus subhirtella', 'prunus virginiana', 'prunus yedoensis', 'pseudolarix amabilis', 'ptelea trifoliata', 'pyrus calleryana', 'quercus acutissima', 'quercus alba', 'quercus bicolor', 'quercus cerris', 'quercus coccinea', 'quercus falcata', 'quercus imbricaria', 'quercus macrocarpa', 'quercus marilandica', 'quercus michauxii', 'quercus montana', 'quercus muehlenbergii', 'quercus nigra', 'quercus palustris', 'quercus phellos', 'quercus robur', 'quercus rubra', 'quercus shumardii', 'quercus stellata', 'quercus velutina', 'quercus virginiana', 'robinia pseudo-acacia', 'salix babylonica', 'salix caroliniana', 'salix matsudana', 'salix nigra', 'sassafras albidum', 'staphylea trifolia', 'stewartia pseudocamellia', 'styrax japonica', 'styrax obassia', 'syringa reticulata', 'taxodium distichum', 'tilia americana', 'tilia cordata', 'tilia europaea', 'tilia tomentosa', 'toona sinensis', 'tsuga canadensis', 'ulmus americana', 'ulmus glabra', 'ulmus parvifolia', 'ulmus pumila', 'ulmus rubra', 'zelkova serrata']
+
+
+def decode_image(img_str):
+    """Decode the base64 image string into a PIL image."""
+    img_bytes = base64.b64decode(img_str)
+    img = Image.open(io.BytesIO(img_bytes)).convert('RGB')
+    return img
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    try:
+        # Decode the image
+        img = decode_image(data['image'])
+        # Transform the image
+        img_tensor = transform(img)
+        # Add batch dimension
+        img_tensor = img_tensor.unsqueeze(0)
+        
+        # Prediction
+        with torch.no_grad():
+            outputs = model(img_tensor)
+            _, predicted = torch.max(outputs.data, 1)
+            predicted_species = predicted.item()
+            predicted_species_name = class_labels[predicted_species]  # Map index to name
+
+        
+        print("predicted species: ", predicted_species_name)
+        return jsonify({'species': predicted_species_name})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+
+API_URL = "https://api-inference.huggingface.co/models/stanghong/leaf-disease-id"
+headers = {"Authorization": "Bearer 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"}
+
+@app.route('/disease', methods=['POST'])
+def disease(): 
+    data = request.json
+    try: 
+        image_data = base64.b64encode(data)
+        response = requests.post(API_URL, headers=headers, data=image_data)
+
+        responsedict = {}
+        for element in response.json(): 
+            responsedict[element['label']] = element['score']
+
+        print("disease: ", responsedict)
+        return jsonify(responsedict)
+
+    except Exception as e: 
+        return jsonify({'error': str(e)})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)  # Run the server on port 5000
+
